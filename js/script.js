d2876afab242b2c1a6a1203c9cb9b89e9fa06977
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-28 21:48:52 +0200

    base project
    boilerplate index
    
    javascript logic to create mainDiv inside body
    
    empty css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Get body
+const body = document.querySelector("body");
+
+// Set vars to track numbers of rows and cols
+const rows = 16;
+const columns = 16;
+
+// Create main div
+const mainDiv = document.createElement("div");
+
+// Append mainDiv to body
+body.appendChild(mainDiv);
